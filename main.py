5f932db8bddc57b609a592c8dfb2e96d88548785
Sophiya-ai <sophiyabr@gmail.com>
2024-10-04 15:15:17 +0200

    add target

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random as r
 
 pygame.init()
 
@@ -7,7 +8,15 @@ screen_height =600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/ba.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/i.jpg")
+target_width = 50
+target_height = 50
+target_x = r.randint(0, screen_width - target_width)
+target_y = r.randint(0, screen_height - target_height)
+color = (r.randint(0,255),r.randint(0,255),r.randint(0,255))
 
 
 running = True
