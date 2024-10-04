012ec9900491786693276391153644c92e50f25e
Sophiya-ai <sophiyabr@gmail.com>
2024-10-04 14:01:55 +0200

    add const-display

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import pygame
 
 pygame.init()
 
-running = True
+screen_width = 800
+screen_height =600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("")
+
+
 running = True
 while running:
     pass
