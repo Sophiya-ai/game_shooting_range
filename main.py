e8de5cebc71f19c41dea601267813e5d17cb5456
Sophiya-ai <sophiyabr@gmail.com>
2024-10-05 07:27:53 +0200

    addition of explanations

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 import random as r
 
+# Инициализация Pygame
 pygame.init()
 
+# Задаем размеры и вид окна
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/ba.jpg")
 pygame.display.set_icon(icon)
 
+# Задаем параметры и вид мишени
 target_img = pygame.image.load("img/i.jpg")
 target_width = 50
 target_height = 50
 target_x = r.randint(0, SCREEN_WIDTH - target_width)
 target_y = r.randint(0, SCREEN_HEIGHT - target_height)
+
+# Задаем рандомную переменную цвета экрана
 color = (r.randint(0,255),r.randint(0,255),r.randint(0,255))
 
+# Основной игровой цикл
 running = True
 while running:
     screen.fill(color)
@@ -32,4 +37,5 @@ while running:
     screen.blit(target_img, (target_x,target_y))
     pygame.display.update()
 
+# Завершение Pygame
 pygame.quit()
